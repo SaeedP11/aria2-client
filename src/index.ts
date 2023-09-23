fa538fcd16ee794fe4268fdf6667d8a43e673e05
saeed_p11 <saeed_abdi.p11@outlook.com>
2023-09-23 18:58:52 +0330

    impr: export all interfaces and enums and export Aria2Client class as default export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export * from './services/aria2-client.service';
+/* Import */
+// Service
+import Aria2Client from './services/aria2-client.service';
+
+export default Aria2Client;
+export * from './interfaces';
+export * from './enums';
